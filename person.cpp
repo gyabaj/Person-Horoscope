de607eef069c429b2221ee1f6028f5e8410922d1
Areg Karakhanyan <70661831+gyabaj@users.noreply.github.com>
2020-09-12 12:37:10 +0400

    Update person.cpp

--- person.cpp
+++ person.cpp
@@ -14,19 +14,19 @@ namespace PERSON
      {
        switch(bYear % 12)
        {
-         case 0 :   return "Monkey";
-         case 1 :   return "Rooster";
-         case 2 :   return "Dog";
-         case 3 :   return "Pig";
-         case 4 :   return "Rat";
-         case 5 :   return "Ox";
-         case 6 :   return "Tiger";
-         case 7 :   return "Rabbit";
-         case 8 :   return "Dragon";
-         case 9 :   return "Snake";
-         case 10 :  return "Horse";
-         case 11 :  return "Ram";
-         default :  return "Not available value";
+         case 0:   return "Monkey";
+         case 1:   return "Rooster";
+         case 2:   return "Dog";
+         case 3:   return "Pig";
+         case 4:   return "Rat";
+         case 5:   return "Ox";
+         case 6:   return "Tiger";
+         case 7:   return "Rabbit";
+         case 8:   return "Dragon";
+         case 9:   return "Snake";
+         case 10:  return "Horse";
+         case 11:  return "Ram";
+         default:  return "Not available value";
 
        }
      }
